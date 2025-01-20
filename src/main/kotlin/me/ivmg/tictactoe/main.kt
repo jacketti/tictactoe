0715b9b54e892e4c93a2d2a15e929450dcea3c20
Robert Gorter <robert.gorter@ns.nl>
2025-01-20 09:39:00 +0100

    R: put board in a var and print

--- src/main/kotlin/me/ivmg/tictactoe/main.kt
+++ src/main/kotlin/me/ivmg/tictactoe/main.kt
@@ -1,10 +1,12 @@
 package me.ivmg.tictactoe
 
 fun TicTacToe(): String {
-    return "| |\n" +
+    var board = "| |\n" +
             "-+-+-\n" +
             "| |\n" +
             "-+-+-\n" +
             "| |"
+    println(board)
+    return board
 }
 
